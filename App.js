7adfe013db25c30e6c7064999a8618fd8c2f66c2
Wilson Lopez <wiltrouble@gmail.com>
2022-04-22 11:40:16 -0400

    restaurant item, restaurant List

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+
+import RestaurantItem from "./src/components/RestaurantItem";
+import restaurants from "./assets/data/restaurants.json";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList
+        data={restaurants}
+        renderItem={({ item }) => <RestaurantItem restaurant={item} />}
+        showsVerticalScrollIndicator={false}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    paddingVertical: 40 //temporal
   },
 });
---
+++ src/components/RestaurantItem/index.js
@@ -0,0 +1,61 @@
+import { Text, View, Image, StyleSheet } from "react-native";
+
+const RestaurantItem = ({ restaurant }) => {
+  return (
+    <View style={styles.restaurantContainer}>
+      <Image
+        style={styles.image}
+        source={{
+          uri: restaurant.image,
+        }}
+      />
+      <View style={styles.row}>
+      <View>
+      <Text style={styles.title}>{restaurant.name}</Text>
+      <Text style={styles.subtitle}>
+        ${restaurant.deliveryFee} &#8226; {restaurant.minDeliveryTime} -{" "}
+        {restaurant.maxDeliveryTime} minutes
+      </Text>
+      </View>
+      <View style={styles.rating}>
+        <Text>{restaurant.rating}</Text>
+      </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  restaurantContainer: {
+    width: "100%",
+    marginVertical: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 5 / 3,
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  subtitle: {
+    color: "grey",
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  rating: {
+    marginLeft: 'auto',
+    backgroundColor: 'gray',
+    width: 30,
+    height: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20
+  }
+});
+
+export default RestaurantItem;
